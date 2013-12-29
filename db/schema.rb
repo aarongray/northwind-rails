6a7280b19c825e0bb16dd921c2e814fdc3fed451
Aaron Gray <aaron.gray@gradesfirst.com>
2013-12-29 13:41:19 -0600

    Add Products.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131229193432) do
+ActiveRecord::Schema.define(version: 20131229194058) do
 
   create_table "categories", force: true do |t|
     t.string   "category_name"
@@ -81,6 +81,20 @@ ActiveRecord::Schema.define(version: 20131229193432) do
     t.string   "ship_region"
     t.string   "ship_postal_code"
     t.string   "ship_country"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "products", force: true do |t|
+    t.string   "product_name"
+    t.integer  "supplier_id"
+    t.integer  "category_id"
+    t.string   "quantity_per_unit"
+    t.float    "unit_price"
+    t.integer  "units_in_stock"
+    t.integer  "units_on_order"
+    t.integer  "reorder_level"
+    t.boolean  "discontinued"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
