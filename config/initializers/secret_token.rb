0200460e239bf6f8608079446fc1d5d13e815b68
Aaron Gray <aaron.gray@gradesfirst.com>
2013-12-28 22:52:00 -0600

    Initial commit to install Rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Northwind::Application.config.secret_key_base = 'b9ab2cb66a5222312744216cca0b331d4294f5f1695db2ee81db729f1204bb067d65bd1b473be63666a02187cc6282b3b73c767c3389375526399c335f17a041'
