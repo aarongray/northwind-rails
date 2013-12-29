4ad5d1174365ee5b5a3eac8e8b3db188ffe00190
Aaron Gray <aaron.gray@gradesfirst.com>
2013-12-29 13:44:32 -0600

    Add Suppliers.

---
+++ db/migrate/20131229194415_create_suppliers.rb
@@ -0,0 +1,19 @@
+class CreateSuppliers < ActiveRecord::Migration
+  def change
+    create_table :suppliers do |t|
+      t.string :company_name
+      t.string :contact_name
+      t.string :contact_title
+      t.string :address
+      t.string :city
+      t.string :region
+      t.string :postal_code
+      t.string :country
+      t.string :phone
+      t.string :fax
+      t.text :home_page
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131229194211) do
+ActiveRecord::Schema.define(version: 20131229194415) do
 
   create_table "categories", force: true do |t|
     t.string   "category_name"
@@ -102,6 +102,22 @@ ActiveRecord::Schema.define(version: 20131229194211) do
   create_table "shippers", force: true do |t|
     t.string   "company_name"
     t.string   "phone"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "suppliers", force: true do |t|
+    t.string   "company_name"
+    t.string   "contact_name"
+    t.string   "contact_title"
+    t.string   "address"
+    t.string   "city"
+    t.string   "region"
+    t.string   "postal_code"
+    t.string   "country"
+    t.string   "phone"
+    t.string   "fax"
+    t.text     "home_page"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
