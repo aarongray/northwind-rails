3b4a26e6a36d289e6961c5546d1812ed4ae09e83
aarongray <aaron@ponderyourpath.com>
2014-02-27 21:29:18 -0500

    Add home page.

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Northwind::Application.routes.draw do
     resources :suppliers
   end
 
+  root 'pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
