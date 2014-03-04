8336ae02af99fdb1881371f03930633717a193ac
aarongray <aaron@ponderyourpath.com>
2014-03-04 10:38:27 -0500

    Add pagination to the Suppliers grid.

--- app/assets/javascripts/controllers/suppliers-controller.js
+++ app/assets/javascripts/controllers/suppliers-controller.js
@@ -3,6 +3,24 @@
 App.controller('SuppliersController', ['$scope', 'Supplier',
   function ($scope, Supplier) {
     $scope.suppliers = Supplier.query();
+    $scope.maxItemsPerPage = 10;
+    $scope.currentPage = 0;
+    $scope.numberOfPages = function() {
+      return Math.ceil($scope.suppliers.length/$scope.maxItemsPerPage);
+    }
+    $scope.testPages = $scope.numberOfPages;
+
+    $scope.setPage = function () {
+      $scope.currentPage = this.paginator;
+    };
+
+    $scope.previousPage = function () {
+      $scope.currentPage--;
+    };
+
+    $scope.nextPage = function () {
+      $scope.currentPage++;
+    };
   }
 ]);
 
