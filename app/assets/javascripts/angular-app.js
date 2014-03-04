2b35ee0cd56eb08bb50274bd522f9904f23ab330
aarongray <aaron@ponderyourpath.com>
2014-03-03 21:55:02 -0500

    Add pagination and an filter to assist.
    Use it on the Orders grid.

--- app/assets/javascripts/angular-app.js
+++ app/assets/javascripts/angular-app.js
@@ -3,3 +3,22 @@
 
 /* App Module */
 var App = angular.module('northwindApp', ['ngResource']);
+
+
+// App filters can be used in your views. This one will determine where to begin
+// loading grid data for pagination purposes. Angular already provides a limitTo
+// filter, which we will use for the end of the grid data per page, but we need
+// to roll our own for the beginning of the data.
+//
+// Usage:
+//   <tr ng-repeat="order in orders | startFrom: currentPage*maxItemsPerPage | limitTo: maxItemsPerPage">
+//     <td>{{order.id}}</td>
+//     <td>{{order.customer_id}}</td>
+//   </tr>
+App.filter('startFrom', function() {
+    return function(input, start) {
+        start = +start; //parse to int
+        return input.slice(start);
+    }
+});
+
--- app/assets/javascripts/controllers/orders-controller.js
+++ app/assets/javascripts/controllers/orders-controller.js
@@ -3,6 +3,24 @@
 App.controller('OrdersController', ['$scope', 'Order',
   function ($scope, Order) {
     $scope.orders = Order.query();
+    $scope.maxItemsPerPage = 10;
+    $scope.currentPage = 0;
+    $scope.numberOfPages = function() {
+      return Math.ceil($scope.orders.length/$scope.maxItemsPerPage);
+    }
+    $scope.testPages = $scope.numberOfPages;
+
+    $scope.setPage = function () {
+      $scope.currentPage = this.paginator;
+    };
+
+    $scope.previousPage = function () {
+      $scope.currentPage--;
+    };
+
+    $scope.nextPage = function () {
+      $scope.currentPage++;
+    };
   }
 ]);
 
