719b6679431d6718e7943d2526b71ea080275974
aarongray <aaron@ponderyourpath.com>
2014-03-04 10:12:49 -0500

    Add pagination to the Products grid.

--- app/assets/javascripts/controllers/products-controller.js
+++ app/assets/javascripts/controllers/products-controller.js
@@ -3,6 +3,24 @@
 App.controller('ProductsController', ['$scope', 'Product',
   function ($scope, Product) {
     $scope.products = Product.query();
+    $scope.maxItemsPerPage = 10;
+    $scope.currentPage = 0;
+    $scope.numberOfPages = function() {
+      return Math.ceil($scope.products.length/$scope.maxItemsPerPage);
+    }
+    $scope.testPages = $scope.numberOfPages;
+
+    $scope.setPage = function () {
+      $scope.currentPage = this.paginator;
+    };
+
+    $scope.previousPage = function () {
+      $scope.currentPage--;
+    };
+
+    $scope.nextPage = function () {
+      $scope.currentPage++;
+    };
   }
 ]);
 
