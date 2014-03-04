957559ef042280f67491bb6eba36fe3bbba62935
aarongray <aaron@ponderyourpath.com>
2014-03-04 10:16:50 -0500

    Add pagination to the Customers grid.

--- app/assets/javascripts/controllers/customers-controller.js
+++ app/assets/javascripts/controllers/customers-controller.js
@@ -3,6 +3,24 @@
 App.controller('CustomersController', ['$scope', 'Customer',
   function ($scope, Customer) {
     $scope.customers = Customer.query();
+    $scope.maxItemsPerPage = 10;
+    $scope.currentPage = 0;
+    $scope.numberOfPages = function() {
+      return Math.ceil($scope.customers.length/$scope.maxItemsPerPage);
+    }
+    $scope.testPages = $scope.numberOfPages;
+
+    $scope.setPage = function () {
+      $scope.currentPage = this.paginator;
+    };
+
+    $scope.previousPage = function () {
+      $scope.currentPage--;
+    };
+
+    $scope.nextPage = function () {
+      $scope.currentPage++;
+    };
   }
 ]);
 
