5cce930ebfe581d8ef3b81506a4329ed063301e6
aarongray <aaron@ponderyourpath.com>
2014-03-04 10:07:06 -0500

    Add pagination to the OrderDetails grid.

--- app/assets/javascripts/controllers/order-details-controller.js
+++ app/assets/javascripts/controllers/order-details-controller.js
@@ -3,6 +3,24 @@
 App.controller('OrderDetailsController', ['$scope', 'OrderDetail',
   function ($scope, OrderDetail) {
     $scope.orderDetails = OrderDetail.query();
+    $scope.maxItemsPerPage = 10;
+    $scope.currentPage = 0;
+    $scope.numberOfPages = function() {
+      return Math.ceil($scope.orderDetails.length/$scope.maxItemsPerPage);
+    }
+    $scope.testPages = $scope.numberOfPages;
+
+    $scope.setPage = function () {
+      $scope.currentPage = this.paginator;
+    };
+
+    $scope.previousPage = function () {
+      $scope.currentPage--;
+    };
+
+    $scope.nextPage = function () {
+      $scope.currentPage++;
+    };
   }
 ]);
 
