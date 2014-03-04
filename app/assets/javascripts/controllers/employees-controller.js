83c084b3f2395ea74b7976e8c6b39620f7d7f0b8
aarongray <aaron@ponderyourpath.com>
2014-03-04 10:23:26 -0500

    Add pagination to the Employees grid.

--- app/assets/javascripts/controllers/employees-controller.js
+++ app/assets/javascripts/controllers/employees-controller.js
@@ -3,5 +3,23 @@
 App.controller('EmployeesController', ['$scope', 'Employee',
   function ($scope, Employee) {
     $scope.employees = Employee.query();
+    $scope.maxItemsPerPage = 10;
+    $scope.currentPage = 0;
+    $scope.numberOfPages = function() {
+      return Math.ceil($scope.employees.length/$scope.maxItemsPerPage);
+    }
+    $scope.testPages = $scope.numberOfPages;
+
+    $scope.setPage = function () {
+      $scope.currentPage = this.paginator;
+    };
+
+    $scope.previousPage = function () {
+      $scope.currentPage--;
+    };
+
+    $scope.nextPage = function () {
+      $scope.currentPage++;
+    };
   }]);
 
